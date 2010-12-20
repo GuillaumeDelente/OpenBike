ca632d8abb95bf8945cc93f79ead1b8a561495d2
Guillaume Delente <guillaume.delente@gmail.com>
2010-12-20 12:51:08 +0100

    Add GPL License

--- src/fr/vcuboid/filter/FilterPreferencesActivity.java
+++ src/fr/vcuboid/filter/FilterPreferencesActivity.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2010 Guillaume Delente
+ *
+ * This file is part of .
+ *
+ * Vcuboid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * Vcuboid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Vcuboid.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package fr.vcuboid.filter;
 
 import android.app.AlertDialog;
